53f059169bb1267501fb68a685b38d5f165715ef
Riley Ge <rlge@live.com>
2022-09-24 20:32:51 +0800

    Merge faces

--- Source/Macad.Exchange/Dxf/DxfBodyImporter.cs
+++ Source/Macad.Exchange/Dxf/DxfBodyImporter.cs
@@ -1,15 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using IxMilia.Dxf;
 using IxMilia.Dxf.Entities;
 using Macad.Common;
+using Macad.Core;
 using Macad.Core.Components;
 using Macad.Core.Geom;
 using Macad.Core.Shapes;
 using Macad.Core.Topology;
 using Macad.Occt;
 using Macad.Occt.Helper;
+using System.Diagnostics;
 
 namespace Macad.Exchange
 {
@@ -37,6 +40,7 @@ namespace Macad.Exchange
             bodies = null;
             var tempBodies = new List<Body>();
             DxfFile reader = DxfFile.Load(fileName);
+            Stopwatch sw = new();
             //int i = 0;
             foreach(var entity in reader.Entities)
             {
@@ -44,11 +48,15 @@ namespace Macad.Exchange
                 {
                     case IxMilia.Dxf.Entities.DxfEntityType.Insert:
                         var insert = entity as DxfInsert;
+                        sw.Start();
                         tempBodies.Add(_CreateBody(insert));
+                        sw.Stop();
                         break;
                 }
                 //i++;
             }
+            string time = sw.Elapsed.ToString();
+            Console.WriteLine(time);
             //while (reader.MoveNext(out cmd))
             //{
             //    switch (cmd)
@@ -115,12 +123,13 @@ namespace Macad.Exchange
         //--------------------------------------------------------------------------------------------------
 
         static Body _CreateBody(DxfInsert dxfInsert)
-        {            
-            
+        {
             var compound = new TopoDS_Compound();
             var builder = new BRep_Builder();
             builder.MakeCompound(compound);
-            int i = 0;
+            //int i = 0;
+            List<TopoDS_Vertex> allPts = new();
+            Dictionary<int, Dictionary<int, TopoDS_Edge>> indexedEdges = new();
             foreach (var entity in dxfInsert.Entities)
             {
                 switch (entity.EntityType)
@@ -129,37 +138,29 @@ namespace Macad.Exchange
                         break;
                     case DxfEntityType.Face:
                         var dxfFace = entity as Dxf3DFace;
-                        var pts = new List<DxfPoint>() {
+                        List<int> ptIndex = AddPoints(allPts, new DxfPoint[] {
                             dxfFace.FirstCorner,
                             dxfFace.SecondCorner,
                             dxfFace.ThirdCorner,
                             dxfFace.FourthCorner
-                        };
-                        var disPts = pts.Distinct();
-                        if (disPts.Count() > 2)
+                        });
+                        var disPtIndex = ptIndex.Distinct().ToList();
+                        if (disPtIndex.Count() > 2)
                         {
-                            var occtPts = disPts.Select(x => new Pnt(x.X, x.Y, x.Z)).ToArray();
-                            //if (i == 2) break;
-                            if (disPts.Count() == 4)
-                            {
-                                var face = MakeFace(occtPts);
-                                builder.Add(compound, face);
-                            }                                                       
-                            else                            
-                                builder.Add(compound, MakeFace(occtPts));                                                                               
+                            builder.Add(compound, MakeFace(allPts, indexedEdges, disPtIndex));
                         }                      
                         break;
                 }
-                i++;
+                //i++;
                 //if (i == 2) break;
             }
             
             var unifiedCompound = new ShapeUpgrade_UnifySameDomain(compound, true, true, true);
-            unifiedCompound.SetLinearTolerance(1);
-            unifiedCompound.SetAngularTolerance(1);
-            unifiedCompound.AllowInternalEdges(true);
+            //unifiedCompound.SetLinearTolerance(1);
+            //unifiedCompound.SetAngularTolerance(1);
+            //unifiedCompound.AllowInternalEdges(true);
             unifiedCompound.Build();
-            var solid = Solid.Create(unifiedCompound.Shape());
+            var solid = Mesh.Create(unifiedCompound.Shape());
             //var unifiedBody = new ShapeUpgrade_UnifySameDomain(solid);
             var body = Body.Create(solid);
             body.Name = dxfInsert.Name;
@@ -170,35 +171,117 @@ namespace Macad.Exchange
             //body.Name = dxfInsert.Name;
             return body;
         }
+
+        private static TopoDS_Shape MakeFace(List<TopoDS_Vertex> allPts, Dictionary<int, Dictionary<int, TopoDS_Edge>> indexedEdges, List<int> disPtIndex)
+        {
+            var edges = new List<TopoDS_Edge>();
+            for (int i = 0; i < disPtIndex.Count; i++)
+            {
+                TopoDS_Edge edge = MakeOrFindEdge(allPts, indexedEdges, disPtIndex[i], disPtIndex[i == disPtIndex.Count - 1 ? 0 : i + 1]);
+                BRep_Builder edgeBuilder = new();
+                edgeBuilder.UpdateEdge(edge, 0.001);
+                edges.Add(edge);
+            }
+            var wire = disPtIndex.Count == 3 ? new BRepBuilderAPI_MakeWire(edges[0], edges[1], edges[2]).Wire() :
+                new BRepBuilderAPI_MakeWire(edges[0], edges[1], edges[2], edges[3]).Wire();
+
+            var face = new BRepBuilderAPI_MakeFace(wire).Face();
+            BRep_Builder faceBuilder = new();
+            faceBuilder.UpdateFace(face, 0.001);
+            return face;
+        }
+
+        private static TopoDS_Edge MakeOrFindEdge(List<TopoDS_Vertex> allPts, Dictionary<int, Dictionary<int, TopoDS_Edge>> indexedEdges, int v1, int v2)
+        {
+            
+            //throw new NotImplementedException();
+            int big = v1 > v2 ? v1 : v2;
+            int small = v1 > v2 ? v2 : v1;
+            indexedEdges.TryGetValue(small, out var bigDic);
+            TopoDS_Edge edge;
+            if(bigDic == null)
+            {
+                edge = MakeEdgeWith2Points(allPts[small], allPts[big]);
+                Dictionary<int, TopoDS_Edge> keyValuePairs = new();
+                keyValuePairs.Add(big, edge);
+                indexedEdges.Add(small, keyValuePairs);
+            }
+            else
+            {
+                bigDic.TryGetValue(big, out edge);
+                if(edge == null)
+                {
+                    edge = MakeEdgeWith2Points(allPts[small], allPts[big]);
+                    bigDic.Add(big, edge);
+                }
+            }
+            return edge;
+        }
+
+        private static TopoDS_Edge MakeEdgeWith2Points(TopoDS_Vertex smallPoint, TopoDS_Vertex bigPoint)
+        {
+            var edge = new BRepBuilderAPI_MakeEdge(smallPoint, bigPoint).Edge();
+            BRep_Builder edgeBuilder = new();
+            edgeBuilder.UpdateEdge(edge, 0.001);
+            return edge;
+        }
+
+        private static List<int> AddPoints(List<TopoDS_Vertex> allPts, DxfPoint[] dxfPoints)
+        {
+            List<int> ptIndex = new();            
+            foreach(var pt in dxfPoints)
+            {
+                Pnt ptt = new Pnt(pt.X, pt.Y, pt.Z);
+                var index = allPts.IndexOfFirst(x => x.Pnt().X == ptt.X && x.Pnt().Y == ptt.Y && x.Pnt().Z == ptt.Z);
+                if (index >= 0) ptIndex.Add(index);
+                else
+                {
+                    BRep_Builder aBuilder = new();
+                    TopoDS_Vertex aVertex = new BRepBuilderAPI_MakeVertex(ptt).Vertex();
+                    aBuilder.UpdateVertex(aVertex, ptt, 0.001);
+                    aBuilder.Dispose();
+                    allPts.Add(aVertex);
+                    ptIndex.Add(allPts.Count - 1);                    
+                }
+            }
+            return ptIndex;
+        }
+
         public static TopoDS_Face MakeFace(Pnt[] points)
         {
             var edges = new List<TopoDS_Edge>();
             for (int i = 0; i < points.Length; i++)
             {
                 //gp_Pnt pt = new gp_Pnt()
-                //BRep_Builder aBuilder = new();
-                //TopoDS_Vertex aVertex = new BRepBuilderAPI_MakeVertex(points[i]).Vertex();                
-                //aBuilder.UpdateVertex(aVertex, points[i], 0.001);
-                //aBuilder.Dispose();
+                BRep_Builder aBuilder = new();
+                TopoDS_Vertex aVertex = new BRepBuilderAPI_MakeVertex(points[i]).Vertex();
+                aBuilder.UpdateVertex(aVertex, points[i], 0.001);
+                aBuilder.Dispose();
 
-                //BRep_Builder bBuilder = new();
-                //TopoDS_Vertex bVertex = new BRepBuilderAPI_MakeVertex(points[i == points.Length - 1 ? 0 : i + 1]).Vertex();
-                //bBuilder.UpdateVertex(bVertex, points[i == points.Length - 1 ? 0 : i + 1], 0.001);                
-                //bBuilder.Dispose();
+                BRep_Builder bBuilder = new();
+                TopoDS_Vertex bVertex = new BRepBuilderAPI_MakeVertex(points[i == points.Length - 1 ? 0 : i + 1]).Vertex();
+                bBuilder.UpdateVertex(bVertex, points[i == points.Length - 1 ? 0 : i + 1], 0.001);
+                bBuilder.Dispose();
                 //aBuilder.TopoDS_TShape(aVertex, aPoint, Precision::Confusion());
                 //aVertex.Orientation(TopAbs_REVERSED);
                 //var v = new BRepBuilderAPI_MakeVertex(points[0]);
-                var edge = new BRepBuilderAPI_MakeEdge(points[i], points[i == points.Length - 1 ? 0 : i + 1]).Edge();
+                //var edge = new BRepBuilderAPI_MakeEdge(points[i], points[i == points.Length - 1 ? 0 : i + 1]).Edge();
+                var edge = new BRepBuilderAPI_MakeEdge(aVertex, bVertex).Edge();
+                //edge.Reverse();
                 BRep_Builder edgeBuilder = new();
                 edgeBuilder.UpdateEdge(edge, 0.001);
                 edges.Add(edge);
-            } 
+            }
+            //edges[2].Reverse();
             var wire = points.Length == 3 ? new BRepBuilderAPI_MakeWire(edges[0], edges[1], edges[2]).Wire() :
                 new BRepBuilderAPI_MakeWire(edges[0], edges[1], edges[2], edges[3]).Wire();
 
             //var edge_fix = new ShapeFix_Edge(edges[0]);
             //var wireFix = new ShapeFix_Wireframe(wire);
-            return new BRepBuilderAPI_MakeFace(wire).Face();
+            var face = new BRepBuilderAPI_MakeFace(wire).Face();
+            BRep_Builder faceBuilder = new();
+            faceBuilder.UpdateFace(face, 0.001);
+            return face;
             //BRep_Builder fBuilder = new();
             //var ff = face.Face();
             //fBuilder.UpdateFace(ff, 0.001);            
